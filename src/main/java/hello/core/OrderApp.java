5d25e3d05a56d36a5d1a4d8e538dfcdf279d23a6
YE P <oeun.park@gmail.com>
2021-11-05 12:55:09 -0400

    Update fixedDiscount and RateDiscount both

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -11,14 +11,20 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+
+//        MemberService memberService = new MemberServiceImpl();
+//        OrderService orderService = new OrderServiceImpl();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOder(memberId, "itemA", 10000);
+        Order order = orderService.createOder(memberId, "itemA", 80000);
 
         System.out.println("Order = " + order);
         System.out.println("order.calculatePrice = "+ order.calculatePrice());
